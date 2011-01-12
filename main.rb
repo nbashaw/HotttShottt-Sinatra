66ab2907490aebf778a27c1f0d1f47ef26966584
Dan Poggi <dan.poggi@gmail.com>
2011-01-12 11:20:49 -0500

    Converted views to HAML/SASS (mostly)

--- main.rb
+++ main.rb
@@ -1,34 +1,23 @@
 require 'rubygems'
 require 'sinatra'
+require 'haml'
 require 'swish'
-#require 'dm-core'
-#require 'dm-migrations'
-
-#DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
-
-#class Leader
-#  include DataMapper::Resource  
-#  property :id,           Serial
-#  property :shot_id,      Integer
-#  property :win_count,    Integer, :default => 1
-#end
 
 get '/' do
   @shot1 = Dribbble::Shot.find(rand(90000) + 100)
   @shot2 = Dribbble::Shot.find(rand(90000) + 100)
-  erb :match
+  haml :match
 end
 
 get '/vote/*' do
   vote_id = params[:splat]
-  # unless leader already exists
-    # create a new leader for that shot in the db with a default of 1 vote
-  # else add one point to it's count
   redirect '/'
 end
 
 get '/leaderboard' do
-  erb :leaderboard
+  haml :leaderboard
 end
 
-#DataMapper.auto_upgrade!
+get '/screen.css' do
+  sass :screen
+end
